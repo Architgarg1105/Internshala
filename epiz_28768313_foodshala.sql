6602c04588571edd3b13eb6c7f15cff65dfb8d16
Architgarg1105 <architgarglmg9.4@gmail.com>
2021-06-02 15:01:28 +0530

    updated

--- epiz_28768313_foodshala.sql
+++ epiz_28768313_foodshala.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.4
+-- version 4.9.0.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Jun 01, 2021 at 01:18 PM
--- Server version: 10.4.17-MariaDB
--- PHP Version: 7.4.13
+-- Host: sql100.byetcluster.com
+-- Generation Time: Jun 02, 2021 at 05:24 AM
+-- Server version: 5.6.48-88.0
+-- PHP Version: 7.2.22
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -18,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `foodshala`
+-- Database: `epiz_28768313_foodshala`
 --
 
 -- --------------------------------------------------------
@@ -33,7 +34,7 @@ CREATE TABLE `add_item` (
   `itemname` varchar(20) NOT NULL,
   `price` int(5) NOT NULL,
   `category` varchar(8) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `add_item`
@@ -63,7 +64,7 @@ CREATE TABLE `customer_details` (
   `customercontactnumber` bigint(10) NOT NULL,
   `address` varchar(80) NOT NULL,
   `preferance` varchar(8) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `customer_details`
@@ -85,7 +86,7 @@ CREATE TABLE `ordered_items` (
   `customerid` int(6) NOT NULL,
   `restaurantid` int(6) NOT NULL,
   `itemid` int(6) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `ordered_items`
@@ -100,7 +101,9 @@ INSERT INTO `ordered_items` (`orderid`, `customerid`, `restaurantid`, `itemid`) 
 (6, 2, 3, 8),
 (7, 3, 1, 3),
 (8, 3, 2, 6),
-(9, 3, 3, 9);
+(9, 3, 3, 9),
+(10, 1, 1, 3),
+(11, 1, 1, 2);
 
 -- --------------------------------------------------------
 
@@ -115,7 +118,7 @@ CREATE TABLE `restaurant_details` (
   `restaurantname` varchar(30) NOT NULL,
   `restaurantcontactnumber` bigint(10) NOT NULL,
   `address` varchar(80) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `restaurant_details`
@@ -174,7 +177,7 @@ ALTER TABLE `customer_details`
 -- AUTO_INCREMENT for table `ordered_items`
 --
 ALTER TABLE `ordered_items`
-  MODIFY `orderid` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `orderid` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `restaurant_details`
